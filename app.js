506696593bb8b9728eb904e9f34e8aabfc344c00
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2024-11-10 14:01:57 +0530

    Added host name

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
-    app.listen(port, () =>
+    app.listen(port, '0.0.0.0', () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {

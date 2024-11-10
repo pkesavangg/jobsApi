0057bb56107f43a1dbe17202a0e57180dac713a1
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2024-11-10 13:31:08 +0530

    Added swagger documentation

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const cors = require('cors')
 
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
+
 const express = require('express');
 const app = express();
 
@@ -32,8 +40,11 @@ app.use(xss())
 
 
 app.get('/', (req, res) => {
-  res.send('Jobs API')
+  res.send('<h1>Jobs AsPI</h1><a href="/api-docs">Documentation</a>')
 })
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 
 app.use('/api/v1/auth', authRoutes);
 
